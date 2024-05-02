093816fbb7704ecdcf224af74e2dc7f7ac6d3f93
rdoyle99 <rpdoyle1@gmail.com>
2024-05-02 10:07:39 +0800

    first

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,310 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import {useState, useEffect} from "react";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ["latin"] });
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
+
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 export default function Home() {
+  const [messages, setMessages] = useState([
+    {
+      message:
+        "Let's start by learning more about your accident. What happened?",
+      sentTime: "just now",
+      sender: "AttorneyAI",
+    },
+  ]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const handleSendRequest = async (message) => {
+    const newMessage = {
+      message,
+      direction: "outgoing",
+      sender: "user",
+    };
+
+    setMessages((prevMessages) => [...prevMessages, newMessage]);
+    setIsTyping(true);
+
+    try {
+      const response = await processMessageToChatGPT([...messages, newMessage]);
+      const content = response.choices[0]?.message?.content;
+      if (content) {
+        const chatGPTResponse = {
+          message: content,
+          sender: "AttorneyAI",
+        };
+        setMessages((prevMessages) => [...prevMessages, chatGPTResponse]);
+      }
+    } catch (error) {
+      console.error("Error processing message:", error);
+    } finally {
+      setIsTyping(false);
+    }
+  };
+
+  async function processMessageToChatGPT(chatMessages) {
+    const apiMessages = chatMessages.map((messageObject) => {
+      const role = messageObject.sender === "AttorneyAI" ? "assistant" : "user";
+      return {role, content: messageObject.message};
+    });
+
+    const apiRequestBody = {
+      model: "gpt-4-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "This GPT, acting as a personal injury attorney bot, is tailored to handle a broad range of injury cases. It asks clear, direct questions, one at a time, to gather essential details about the injury and the circumstances. Once a user indicates a preferred path—such as negotiation or legal action—the bot suggests that it has an attorney who can assist with this and asks for contact information to arrange a free consultation. Throughout, it communicates at a 3rd to 5th grade reading level to ensure accessibility and clarity. The bot provides information on potential financial compensation beyond insurance offers, emphasizing a professional, understanding, and straightforward approach.",
+        },
+        {
+          role: "assistant",
+          content:
+            "Let's start by learning more about your accident. What happened?",
+        },
+        ...apiMessages,
+      ],
+    };
+
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + API_KEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(apiRequestBody),
+    });
+
+    return response.json();
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <>
+      <Head>
+        <title>
+          AttorneyAI: Expert AI Legal Assistance for Personal Injury Cases
+        </title>
+        <meta
+          name="description"
+          content="Get expert legal advice with AttorneyAI, your AI-powered assistant for navigating personal injury claims. Discover how our AI can guide you through the legal process, helping you understand your rights and options."
         />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="AttorneyAI: Expert AI Legal Assistance for Personal Injury Cases"
+        />
+        <meta
+          property="og:description"
+          content="Explore AI-driven legal advice for personal injury with AttorneyAI. Understand your rights and make informed decisions with the help of AI."
+        />
+        <meta property="og:image" content="https://i.imgur.com/8UNngfq.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Head>
+
+      <div className="relative h-full w-full items-center">
+        <div
+          className="absolute pattern-dots pattern-gray-500 pattern-bg-white 
+  pattern-size-6 pattern-opacity-10 w-full h-screen bg-black z-0"
+        ></div>
+        <nav
+          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
+          <div className="flex lg:flex-1 ">
+            <a href="/" className="-m-2 p-1.5 bg-white z-10">
+              <span className="text-xl font-bold text-black ">Attorney AI</span>
+            </a>
+          </div>
+        </nav>
+        <div className="px-6 pt-12 sm:pt-12 lg:px-8 bg-white z-10 block opacity-100">
+          <div className="mx-auto max-w-2xl text-center bg-white z-10 block opacity-100">
+            <h1 className="text-4xl font-bold tracking-tight text-black sm:text-6xl bg-white z-10 block opacity-100">
+              Attorney AI
+            </h1>
+            <h2 className="mt-4 text-lg leading-8 text-gray-700 font-bold">
+              Let's discuss your case and see what your injury could be worth -
+              even if insurance has already offered you a settlement.
+            </h2>
+          </div>
+        </div>
+
+        <div className="mx-8 md:mx-64 rounded  border border-r-2 border-b-2 border-black mt-6 h-1/2">
+          <MainContainer style={{height: "400px"}}>
+            <ChatContainer>
+              <MessageList
+                scrollBehavior="smooth"
+                typingIndicator={
+                  isTyping ? (
+                    <TypingIndicator content="AttorneyAI is typing" />
+                  ) : null
+                }
+              >
+                {messages.map((message, i) => {
+                  const messageClass =
+                    message.sender === "AttorneyAI"
+                      ? "bg-gray-300 text-left flex justify-start"
+                      : "bg-blue-100 text-right flex justify-end";
+                  return (
+                    <div
+                      key={i}
+                      className={`flex w-full ${
+                        message.sender === "AttorneyAI"
+                          ? "justify-start"
+                          : "justify-end"
+                      }`}
+                    >
+                      <p
+                        className={`${messageClass} p-2 rounded-lg max-w-xs mx-2 my-1`}
+                      >
+                        {message.message}
+                      </p>
+                    </div>
+                  );
+                })}
+              </MessageList>
+              <MessageInput
+                placeholder="Send a Message"
+                onSend={handleSendRequest}
+              />
+            </ChatContainer>
+          </MainContainer>
+        </div>
+        <div className="faq-section bg-white py-8 mt-8 mx-8 md:mx-64">
+          <div className="container mx-auto px-6">
+            <h2 className="text-3xl font-bold text-center mb-6">
+              Frequently Asked Questions about Personal Injury Law and
+              AttorneyAI
+            </h2>
+
+            <div className="faq-item mb-8">
+              <h3 className="text-xl font-semibold">What is AttorneyAI?</h3>
+              <p>
+                AttorneyAI is a cutting-edge AI-driven platform tailored to
+                assist individuals in navigating personal injury cases. It
+                leverages advanced AI technology to provide personalized legal
+                advice, helping users understand complex legal scenarios related
+                to personal injuries.
+              </p>
+            </div>
+
+            <div className="faq-item mb-8">
+              <h3 className="text-xl font-semibold">
+                How can AttorneyAI help with my personal injury claim?
+              </h3>
+              <p>
+                AttorneyAI uses sophisticated AI algorithms trained on extensive
+                legal databases to analyze your personal injury case details and
+                provide precise, contextually relevant advice. This can help you
+                understand your rights, potential compensation, and the steps
+                necessary for pursuing your claim.
+              </p>
+            </div>
+
+            <div className="faq-item mb-8">
+              <h3 className="text-xl font-semibold">
+                Who can benefit from using AttorneyAI in personal injury cases?
+              </h3>
+              <p>
+                Individuals who have suffered personal injuries and are seeking
+                legal advice, law students interested in personal injury law,
+                legal professionals seeking quick references, or anyone needing
+                guidance on personal injury procedures can benefit from
+                AttorneyAI.
+              </p>
+            </div>
+
+            <div className="faq-item mb-8">
+              <h3 className="text-xl font-semibold">
+                Is AttorneyAI a substitute for a personal injury lawyer?
+              </h3>
+              <p>
+                While AttorneyAI provides valuable legal information and
+                guidance, it is not a substitute for a qualified personal injury
+                lawyer. We recommend consulting with a professional for critical
+                matters involving complex legal proceedings or significant
+                financial implications.
+              </p>
+            </div>
+
+            <div className="faq-item mb-8">
+              <h3 className="text-xl font-semibold">
+                How accurate is the personal injury advice provided by
+                AttorneyAI?
+              </h3>
+              <p>
+                AttorneyAI aims to deliver the most accurate and up-to-date
+                legal information possible. However, users should consider
+                verifying the advice through additional legal consultations,
+                especially in complex personal injury cases.
+              </p>
+            </div>
+
+            <div className="faq-item mb-8">
+              <h3 className="text-xl font-semibold">
+                Is there a cost to use AttorneyAI for personal injury advice?
+              </h3>
+              <p>
+                AttorneyAI offers various services, some of which may be
+                available for free. For more comprehensive features or detailed
+                legal advice, a subscription or payment may be required. Check
+                our platform for detailed pricing and service options.
+              </p>
+            </div>
+
+            <div className="faq-item mb-8">
+              <h3 className="text-xl font-semibold">
+                How does AttorneyAI protect my personal information in personal
+                injury cases?
+              </h3>
+              <p>
+                AttorneyAI is equipped with robust security measures to
+                safeguard your data. All interactions are encrypted, ensuring
+                that your personal injury case details remain confidential and
+                secure.
+              </p>
+            </div>
+
+            <div className="faq-item mb-8">
+              <h3 className="text-xl font-semibold">
+                Can AttorneyAI assist with legal documentation for personal
+                injury claims?
+              </h3>
+              <p>
+                Yes, AttorneyAI can help draft and review basic legal documents
+                related to personal injury claims. For more complex documents,
+                we advise seeking professional legal help.
+              </p>
+            </div>
+
+            <div className="faq-item mb-8">
+              <h3 className="text-xl font-semibold">
+                How do I start using AttorneyAI for my personal injury case?
+              </h3>
+              <p>
+                Getting started is easy. Visit our website, sign up if
+                necessary, and you can immediately begin to input details of
+                your personal injury case to receive tailored legal advice
+                through our interactive chat interface.
+              </p>
+            </div>
+          </div>
+        </div>
+        <footer className="text-center py-4 mt-8 bg-gray-100">
+          <p>
+            © Copyright {new Date().getFullYear()} -- ai-injury-attorney.com --
+            all rights reserved.
+          </p>
+        </footer>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,29 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    // defaults to these values
+    patterns: {
+      opacities: {
+        100: "1",
+        80: ".80",
+        60: ".60",
+        40: ".40",
+        20: ".20",
+        10: ".10",
+        5: ".05",
+      },
+      sizes: {
+        1: "0.25rem",
+        2: "0.5rem",
+        4: "1rem",
+        6: "1.5rem",
+        8: "2rem",
+        16: "4rem",
+        20: "5rem",
+        24: "6rem",
+        32: "8rem",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-bg-patterns")],
 };
