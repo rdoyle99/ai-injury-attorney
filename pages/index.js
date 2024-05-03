b532cbeb9af073bc73895d2a16411f29cf89a43c
rdoyle99 <rpdoyle1@gmail.com>
2024-05-03 13:05:39 +0800

    somethin

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import {useState, useEffect} from "react";
 import Head from "next/head";
-
+//comment for no reason
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import {
   MainContainer,
